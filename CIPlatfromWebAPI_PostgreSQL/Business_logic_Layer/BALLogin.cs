7972419629369e7d70eeecbfd68e96f4c0b2266b
Krushna Rathod <rathodkrushna2405@gmail.com>
2024-07-19 16:44:42 +0530

    first

---
+++ CIPlatfromWebAPI_PostgreSQL/Business_logic_Layer/BALLogin.cs
@@ -0,0 +1,60 @@
+﻿using Data_Access_Layer;
+using Data_Access_Layer.JWTService;
+using Data_Access_Layer.Repository.Entities;
+
+namespace Business_logic_Layer
+{
+    public class BALLogin
+    {
+        private readonly DALLogin _dalLogin;
+        private readonly JwtService _jwtService;
+        ResponseResult result = new ResponseResult();
+
+        public BALLogin(DALLogin dalLogin, JwtService jwtService)
+        {
+            _dalLogin = dalLogin;
+            _jwtService = jwtService;
+        }
+
+        public ResponseResult LoginUser(Login login)
+        {
+            try
+            {
+                User userObj = _dalLogin.LoginUser(login);
+
+                if (userObj != null)
+                {
+                    if (userObj.Message == "Login Successfully")
+                    {
+                        result.Message = userObj.Message;
+                        result.Result = ResponseStatus.Success;
+                        result.Data = _jwtService.GenerateToken(
+                            userObj.Id.ToString(),
+                            userObj.FirstName,
+                            userObj.LastName,
+                            userObj.PhoneNumber,
+                            userObj.EmailAddress,
+                            userObj.UserType,
+                            userObj.UserImage
+                        );
+                    }
+                    else
+                    {
+                        result.Message = userObj.Message;
+                        result.Result = ResponseStatus.Error;
+                    }
+                }
+                else
+                {
+                    result.Message = "Error in Login";
+                    result.Result = ResponseStatus.Error;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
